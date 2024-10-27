0cb9eb40a32911221f397ada8483a118ef901c9c
theboredkb <jiayangkb@gmail.com>
2024-10-27 16:21:12 -0700

    added initial code file structure

---
+++ cmd/cli/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+)
+
+func main() {
+	fmt.Printf("hello world")
+}
